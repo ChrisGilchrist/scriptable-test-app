08b793ef19da06fab0ffeb993ec29ae7b25f3c15
Chris Gilchrist <gilly@sgnet.co.uk>
2020-11-22 20:01:43 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,63 +1,33 @@
-let items = await loadItems()
+// First load the items and wait until it's done
+//let items = await loadItems();
 
 if (config.runsInWidget) {
-  let widget = createWidget(items)
-  Script.setWidget(widget)
-  Script.complete()
-} else {
-  let item = items[0]
-  Safari.open(item.url)
+    // Tell the widget on the Home Screen to show our ListWidget Instance.
+    //let widget = await createWidget(items);
+    createWidget();
+    Script.setWidget(widget);
 }
 
+// Function to create widget
 function createWidget(items) {
-  let item = items[0]
-  let authors = item.authors.map(a => {
-    return a.name
-  }).join(", ")
-  let rawDate = item["date_published"]
-  let date = new Date(Date.parse(rawDate))
-  let df = new DateFormatter()
-  df.useFullDateStyle()
-  df.useShortTimeStyle()
-  let strDate = df.string(date)
-  let w = new ListWidget()
-  w.backgroundColor = new Color("#b00a0f")
-  w.centerAlignContent()
-  let titleTxt = w.addText(item.title)
-  titleTxt.applyHeadlineTextStyling()
-  titleTxt.textColor = Color.white()
-  let authorsTxt = w.addText("by " + authors)
-  authorsTxt.applyBodyTextStyling()
-  authorsTxt.textColor = Color.white()
-  authorsTxt.textOpacity = 0.8
-  let dateTxt = w.addText(strDate)
-  dateTxt.applyBodyTextStyling()
-  dateTxt.textColor = Color.white()
-  dateTxt.textOpacity = 0.8
-  return w
-}
-  
+    let widget = new ListWidget();
+    widget.backgroundColor = new Color("#b00a0f");
+    widget.addSpacer();
+
+    let title = 'Hello World';
+    let titleElement = widget.addText(title);
+    titleElement.font = OFont.boldSystemFont(title);
+    titleElement.textColor = Color.white();
+    
+    // Adds spacing under the title
+    widget.addSpacer(8);
+
+    return widget;
+};
+
+// Function to load the data displayed in the widget
 async function loadItems() {
-  let url = "https://macstories.net/feed/json"
-  let req = new Request(url)
-  let json = await req.loadJSON()
-  return json.items
-}
 
-function extractImageURL(item) {
-  let regex = /<img src="(.*)" alt="/
-  let html = item["content_html"]
-  let matches = html.match(regex)
-  if (matches && matches.length >= 2) {
-    return matches[1]
-  } else {
-    return null
-  }
-}
 
-function decode(str) {
-  let regex = /&#(\d+);/g
-  return str.replace(regex, (match, dec) => {
-    return String.fromCharCode(dec)
-  })
+
 }
