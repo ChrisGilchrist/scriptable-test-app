5f8a07d5efd425e47cb21bdf2473d6100bbff2f5
Chris Gilchrist <gilly@sgnet.co.uk>
2020-11-22 20:57:17 +0000

    Update test.js

--- test.js
+++ test.js
@@ -1,15 +1,18 @@
 // First load the items and wait until it's done
-//let items = await loadItems();
+let items = await loadItems();
 
 if (config.runsInWidget) {
     // Tell the widget on the Home Screen to show our ListWidget Instance.
-    //let widget = await createWidget(items);
-    createWidget();
+    
+    // Get the first item in the data list
+    let item = items.data[0];
+    let widget = await createWidget(item);
+    //createWidget();
     Script.setWidget(widget);
 }
 
 // Function to create widget
-function createWidget(items) {
+async function createWidget(item) {
     let widget = new ListWidget();
     widget.backgroundColor = new Color("#b00a0f");
     widget.addSpacer();
@@ -28,6 +31,10 @@ function createWidget(items) {
 // Function to load the data displayed in the widget
 async function loadItems() {
 
-
+    let data_url = "https://api.coronavirus.data.gov.uk/v1/data?filters=areaType=nation;areaName=england&structure={%22date%22:%22date%22,%22newCases%22:%22newCasesByPublishDate%22}";
+    let req = new Request(url);
+    let json = await req.loadJSON();
+    return json.items();
+    
 
 }
